324a53b173b8e636baa89b5f07d2bf0ca922befd
John Joseph Bachir <j@jjb.cc>
2013-04-06 20:13:01 -0400

    `rails new .` with rails 3.2.13

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RspecExpectationsIssue232::Application.config.secret_token = 'e22b5b741c0ff3351ce4efba08cc13e7d3572d540d268ead046ea8e2de2791d6a8b003da8a54632ccb223f1102cf7b272b9f8f1837ee65e158085e3f6fe7d253'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RspecExpectationsIssue232::Application.config.session_store :cookie_store, key: '_rspec-expectations-issue-232_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RspecExpectationsIssue232::Application.config.session_store :active_record_store
